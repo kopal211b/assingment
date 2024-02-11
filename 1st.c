bd2bc535338f37a89a23aee92e1bfe7a1f532224
kopal211b <159139470+kopal211b@users.noreply.github.com>
2024-02-11 15:41:54 +0530

    Update Palindrome.c
    Program to check Palindrome number.

--- 1st.c
+++ 1st.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
+//Program for Palindrome number
+int pal(int n){
+    int temp=n,rev=0,rem;
+    while(n!=0){
+        rem= n%10;
+        rev=rev*10+rem;
+        n /=10;
+    }
+    (temp==rev)? printf("Palindrome"):printf("Not Palindrom");
+}
+
 int main(){
-  int n;
-  scanf("%d",&n);
-  printf("\n %d",n);
-  return 0;
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    pal(num);  
+     return 0;
 }
