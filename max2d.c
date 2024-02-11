f13aab17620d049c17a86db871b9cd999fddc87a
kopal211b <159139470+kopal211b@users.noreply.github.com>
2024-02-11 21:18:30 +0530

    Create max2d.c
    Minimum element from a particular row in 2-d array

---
+++ max2d.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n, m,row;
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+    printf("Enter number of columns: ");
+    scanf("%d", &m);
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for(int j=0;j<m;j++){
+        printf("Enter element at %d %d: ", i+1,j+1);
+        scanf("%d", &a[i][j]);
+        }
+    }
+    int max=a[0][0];
+    printf("Enter row number: ");
+    scanf("%d", &row);
+    for(int j=0;j<m;j++){
+        if (max<a[row-1][j]){
+            max=a[row-1][j];
+        }
+        }
+    printf("Max element in row %d is: %d ",row,max);
+    
+    return 0;
+}
