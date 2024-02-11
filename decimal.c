381822a8e019800d11d7890d545d892f3ba0ef8f
kopal211b <159139470+kopal211b@users.noreply.github.com>
2024-02-11 17:52:35 +0530

    Create decimal.c
    Binary to decimal

---
+++ decimal.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int binary(int n)
+{ 
+    int num = n; 
+    int d = 0, b= 1, temp = num; 
+ 
+    while (temp) { 
+        int l = temp % 10; 
+ 
+        temp = temp / 10; 
+        d+= l * b; 
+        b = b * 2; 
+    } 
+    printf (" Binary equivalent: %d",  d); 
+
+}
+int main(){
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    binary(num);
+    return 0;
+}
