7c479761b61d5d3db08ca53d77946756def9686a
kopal211b <159139470+kopal211b@users.noreply.github.com>
2024-02-11 15:50:55 +0530

    Update perfect.c

--- perfect.c
+++ perfect.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+int perfect(int num)
+{
+    int sum = 0, rem;
+    for (int i = 1; i < num; i++)
+    {
+        if ((num % i) == 0)
+        {
+            sum = sum + i;
+        }
+    }
+    (sum == num) ? printf("Perfect Number!") : printf("Not a Perfect number!");
+}
+int main(){
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+    perfect(num);
+    return 0;
+}
