ddb252e83548c490bf61ade1cd558255d7aa1e59
kopal211b <159139470+kopal211b@users.noreply.github.com>
2024-02-12 19:38:00 +0530

    Create identity.c
    identity matrix

---
+++ identity.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+void lt(int n, int m, int arr[][m])
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                arr[i][j]=1;
+            }
+            else{
+                arr[i][j]=0;
+            }
+        }
+    }
+        printf("Identity matrix: \n");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                printf("%d ", arr[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    int main()
+    {
+        int n, m, n1, m1;
+        printf("Enter number of rows and columns for square: ");
+        scanf("%d", &n);
+        printf("Enter number of rows and columns for square: ");
+        scanf("%d", &m);
+        int arr[n][m];
+        printf("Enter elements for matrix:\n ");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                printf("Enter element at %d %d: ", i + 1, j + 1);
+                scanf("%d", &arr[i][j]);
+            }
+        }
+        lt(n, m, arr);
+
+        return 0;
+    }
